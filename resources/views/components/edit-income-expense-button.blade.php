c4020c0ba4707c7b7cc19acef25795256bda66ce
dhunganaroshan341 <dhunganaroshan341@gmail.com>
2024-10-25 16:30:12 +0545

    changed the edit

--- resources/views/components/edit-income-expense-button.blade.php
+++ resources/views/components/edit-income-expense-button.blade.php
@@ -1,4 +1,4 @@
-<a name="" id="" class="btn btn-primary"
+<a name="" id="" class=""
     href="{{ $type === 'income' ? route('incomes.edit', ['income' => $id]) : route('expenses.edit', ['expense' => $id]) }}"
     role="button">
     {{-- Edit {{ ucfirst($type) }} --}}
